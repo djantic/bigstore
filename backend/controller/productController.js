996fc1c343a9ce052b496ea1a53318b40240f34e
djordje <djoleantic94@gmail.com>
2022-03-24 19:47:33 +0100

    moved productController from product routes to productController

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,2 +1,20 @@
-import expressAsyncHandler from "express-async-handler";
-import Product from "../models/productModel";
+import expressAsyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+const getProducts = expressAsyncHandler(async (req, res) => {
+  const products = await Product.find({});
+
+  res.json(products);
+});
+
+const getProductById = expressAsyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product is not found');
+  }
+});
+
+export { getProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,17 @@
 import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import {
+  getProductById,
+  getProducts,
+} from '../controller/productController.js';
 
 // fetch all products, /api/products
 
 const router = express.Router();
 
-router.get(
-  '/',
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
+router.route('/').get(getProducts);
 
 // fetch one product, /api/products/:id
 
-router.get(
-  '/:id',
-  expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product is not found');
-    }
-  })
-);
+router.route('/:id').get(getProductById);
 
 export default router;
